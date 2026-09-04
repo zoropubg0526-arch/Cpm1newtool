90eab34905901025fbe3c43ea2464be8f8909611
zoropubg0526-arch <zoropubg0526@gmail.com>
2026-09-04 13:37:53 +0800

    Update source account email in bot.py

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ except: pass
 
 # ✅ UPDATED SOURCE ACCOUNT
 FK = "AIzaSyBW1ZbMiUeDZHYUO2bY8Bfnf5rRgrQGPTM"
-SOURCE_ACCOUNT = ('markryancpm1unlockall7193@gmail.com', 'markryancpm1')
+SOURCE_ACCOUNT = ('markryancpm1unlockall7686@gmail.com', 'markryancpm1')
 LOAD_URL = "https://europe-west1-cp-multiplayer.cloudfunctions.net/GetPlayerRecords3"
 SAVE_URL = "https://europe-west1-cp-multiplayer.cloudfunctions.net/SavePlayerRecordsPartially8"
 RANK_URL = "https://us-central1-cp-multiplayer.cloudfunctions.net/SetUserRating5"
